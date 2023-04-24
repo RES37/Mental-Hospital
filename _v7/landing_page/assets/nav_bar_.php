c8ec5aa2c4c87c6d21f6e4132ac07c25048e50b1
RES37 <127312058+RES37@users.noreply.github.com>
2023-04-24 13:57:05 +0800

    Update nav_bar_.php

--- _v7/landing_page/assets/nav_bar_.php
+++ _v7/landing_page/assets/nav_bar_.php
@@ -3,7 +3,7 @@
         <div class="topnav">
             <a class="split1">demoQratic</a>
             <a class="active1" href="index.php">Home</a>
-            <a class="about-us"href="about-us.php">About Us</a>
+            <a class="about-us"href="about_us.php">About Us</a>
             <!-- <a class="split2" href="#help">Help</a> -->
         </div>
     </nav>
