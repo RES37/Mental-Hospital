1114b79f8844cc725257be3c282bfd5d3ec3e086
RES37 <127312058+RES37@users.noreply.github.com>
2023-04-25 14:29:54 +0800

    Update server_register_form.php

--- _v7/main_page/server_side/server_register/server_register_form.php
+++ _v7/main_page/server_side/server_register/server_register_form.php
@@ -4,10 +4,16 @@
 
         <form action="server_register_action.php" method="post">
                 <label for="server_register_STRAND">Strand:</label>
-                <input type="text" name="server_register_STRAND" id="server_register_STRAND" required><br><br>
+                <select name="server_register_STRAND" id="server_register_STRAND" required>
+                        <option value="it mawd">IT MAWD</option>
+                        <option value="humss">HUMSS</option>
+                        <option value="toper">TOPER</option>
+                        <option value="abm">ABM</option>
+                        <option value="cart">CART</option>
+                </select><br><br>
 
-                <label for="server_register_PASSWORD">Password:</label>
-                <input type="password" name="server_register_PASSWORD" id="server_register_PASSWORD" required><br><br>
+                <label for="server_register_USERNAME">Username:</label>
+                <input type="text" name="server_register_USERNAME" id="server_register_USERNAME" required><br><br>
 
                 <input type="submit" value="Register"></input>
         </form>
