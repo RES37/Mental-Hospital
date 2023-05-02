1811ec92544c3e7236298c5801cb6b886e7351c2
Leifera26 <127314521+Leifera26@users.noreply.github.com>
2023-05-03 00:13:00 +0800

    Add files via upload

---
+++ _v11.5/landing_page/about_us.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Google Fonts: Oswald -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald&display=swap" rel="stylesheet">
+
+    <!-- Google Fonts: Lato -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&display=swap" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="assets/css/about-us.css">
+
+    <title>About us</title>
+</head>
+<body>
+    <?php
+        // Include the navigation bar
+        include 'assets/php/nav_bar.php';
+    ?>
+    <main class="main1">
+        <section>
+            <div class="title">
+                ABOUT US
+            </div>
+            <div class="line1"></div>
+            <div class="description">
+                This project was started by a dedicated research group consisting of 5 <!-- <span onclick="location.href='../main_page/server_side/index.php'" style="text-decoration: none;">members</span> --> that are currently taking the senior high school course of Information Technology in Mobile App and Web Development. We are conducting and prototyping a web-based system that utilizes QR technology to tabulate data and showcase it with graphical visualizations.
+            </div>
+        </section>
+    </main>
+    <main class="main2">
+        <section>
+            <div class="team-info">
+                MEET THE TEAM
+            </div>
+            <div class="line2"></div>
+            <div class="member1">
+                <div class="text1-1">Marcus Dex Vibar</div>
+                <div class="text2-1">Project Manager | Front End Dev | Back End Dev</div>
+                <img class="image1" src="images/marcus.jpg" alt="Random Image">
+            </div>
+            <div class="member2">
+                <div class="text1-2">Jairus Rain Mendoza</div>
+                <div class="text2-2">Assitant Project Manager | Front End Dev</div>
+                <img class="image2" src="images/marcus.jpg" alt="Random Image">
+            </div>
+            <div class="member3">
+                <div class="text1-3">Justin Luague</div>
+                <div class="text2-3">Lead Documentarian</div>
+                <img class="image3" src="images/marcus.jpg" alt="Random Image">
+            </div>
+            <div class="member4">
+                <div class="text1-4">Leonard Dave Mendoza</div>
+                <div class="text2-4">Documentarian</div>
+                <img class="image4" src="images/marcus.jpg" alt="Random Image">
+            </div>
+            <div class="member5">
+                <div class="text1-5">Lisandro Zabalo</div>
+                <div class="text2-5">Documentarian</div>
+                <img class="image5" src="images/marcus.jpg" alt="Random Image">
+            </div>
+        </section>
+    </main>
+    <main class="main3">
+        <section>
+            <div class="vision">
+                Vision
+                <div class="the-vision">
+                    To establish ourselves as highly competent and innovative providers of top-quality products for the industry of information technology. Providing businesses of all shapes and sizes with effective systems while maintaining equal services for all workers and consumers.
+                </div>
+            </div>
+            <div class="mission">
+                Mission
+                <div class="the-mission">
+                    Our mission is to be able to provide authentic, reliable, and user-friendly systems while pursuing our services with the utmost enthusiasm and making use of the latest technology the industry has to offer in order to help everyone achieve their goals.
+                </div>
+            </div>
+        </section>
+    </main>
+    <footer>
+        <!-- Add your footer content here -->
+    </footer>
+</body>
+</html>
